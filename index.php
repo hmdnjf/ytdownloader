6a73b7cca05031b76679ed2b0a8129c18b4ccc95
hmdnjf <hmdnjf86@gmail.com>
2022-08-02 21:49:27 +0430

    make it better

--- index.php
+++ index.php
@@ -1,13 +1,31 @@
 <?php
 include 'functions.php';
 
-$result = file_get_contents('https://api.btclod.com/v1/youtube/extract-infos/?detail=VxvzmE2CI6Y&video=1');
+date_default_timezone_set('Asia/Tehran');
 
-$title = json_decode($result)->data->detail->title;
-echo $title;
-SendMessage('1283437650', $title);
+$path = "https://api.telegram.org/bot5470506972:AAG5haKY329tYlJZRBQmSZ8AWA6k2Lh7I10";
 
-$videos = json_decode($result)->data->videos;
+$update = json_decode(file_get_contents("php://input"), TRUE);
+$isAdmin = false;
+
+$chatId = $update["message"]["chat"]["id"];
+$messageText = $update["message"]["text"];
+$messageId = $update["message"]["message_id"];
+$userId = $update["message"]["from"]["id"];
+$replyId = $update["message"]["reply_to_message"]["message_id"];
+
+if (preg_match("/youtube.com\/watch\?v=(\w+)/", $messageText)) {
+    $videoId = preg_match("/youtube.com\/watch\?v=(\w+)/", $messageText);
+
+    $result = file_get_contents(`https://api.btclod.com/v1/youtube/extract-infos/?detail=$videoId&video=1`);
+
+    $title = json_decode($result)->data->detail->title;
+    echo $title;
+    SendMessage('1283437650', $title);
+
+    $videos = json_decode($result)->data->videos;
+}
+
 
 // foreach ($videos as $item) {
 //     SendMessage('1283437650', ($item->url));
