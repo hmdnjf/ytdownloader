be3d65b1ed361b2c514ec6d77fea3ff2d28fb289
hmdnjf <hmdnjf86@gmail.com>
2022-08-02 22:31:53 +0430

    make it better

--- index.php
+++ index.php
@@ -14,14 +14,14 @@ $userId = $update["message"]["from"]["id"];
 $replyId = $update["message"]["reply_to_message"]["message_id"];
 
 preg_match("/youtube.com\/watch\?v=(\w+)/", 'https://www.youtube.com/watch?v=VxvzmE2CI6Y', $videoId);
-print_r($videoId);
-SendMessage('1283437650', $videoId);
+
+SendMessage('1283437650', $videoId[1]);
 
 
 if (preg_match("/youtube.com\/watch\?v=(\w+)/", $messageText)) {
     preg_match("/youtube.com\/watch\?v=(\w+)/", $messageText, $videoId);
 
-    $result = file_get_contents('https://api.btclod.com/v1/youtube/extract-infos/?detail=' + $videoId + '&video=1');
+    $result = file_get_contents('https://api.btclod.com/v1/youtube/extract-infos/?detail=' + $videoId[1] + '&video=1');
 
     $title = json_decode($result)->data->detail->title;
     echo $title;
