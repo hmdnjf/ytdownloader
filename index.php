02217b98ff65c9fbad3bc6db6b89c78f14bf8a16
hmdnjf <hmdnjf86@gmail.com>
2022-08-02 17:32:07 +0430

    make it better

--- index.php
+++ index.php
@@ -2,4 +2,4 @@
 
 $result = file_get_contents('https://api.btclod.com/v1/youtube/extract-infos/?detail=VxvzmE2CI6Y&video=1');
 
-echo ($result)['data']['videos'];
+echo json_decode($result)['data']['videos'];
