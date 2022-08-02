80f42493516a152e3a8946b6ef3bca5752186620
hmdnjf <hmdnjf86@gmail.com>
2022-08-02 22:28:11 +0430

    make it better

--- index.php
+++ index.php
@@ -12,9 +12,7 @@ $messageText = $update["message"]["text"];
 $messageId = $update["message"]["message_id"];
 $userId = $update["message"]["from"]["id"];
 $replyId = $update["message"]["reply_to_message"]["message_id"];
-
-preg_match("/youtube.com\/watch\?v=(\w+)/", 'https://www.youtube.com/watch?v=VxvzmE2CI6Y', $videoId);
-echo $videoId;
+print_r($videoId);
 SendMessage('1283437650', $videoId);
 
 
