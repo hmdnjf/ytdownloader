719317a1c935d3679ba14d8f58e63ed1d0b493ca
hmdnjf <hmdnjf86@gmail.com>
2022-08-02 18:45:06 +0430

    make it better

--- index.php
+++ index.php
@@ -3,9 +3,11 @@ include 'functions.php';
 
 $result = file_get_contents('https://api.btclod.com/v1/youtube/extract-infos/?detail=VxvzmE2CI6Y&video=1');
 
+$title = json_decode($result)->data->details->title;
+SendMessage('1283437650', $title);
+
 $videos = json_decode($result)->data->videos;
 
-foreach ($videos as $item) {
-    print_r($item->url);
-    SendMessage('1283437650', strval($item->url));
-}
+// foreach ($videos as $item) {
+//     SendMessage('1283437650', ($item->url));
+// }
